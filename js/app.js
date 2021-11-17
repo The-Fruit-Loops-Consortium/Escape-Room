fee201c090d0bc66d3a929a1c7b2a107a4492d6f
David Allan Turner <davidallanturner@gmail.com>
2021-11-16 21:09:30 -0700

    added game.html and added some basic js logic

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let db = [
+ {
+  question: "What is 1+1"
+  , answer: 2
+ }
+ , {
+  question: "What is 2+2"
+  , answer: 4
+ }
+]
+
+let answer =document.prompt(db[0].question);
+
+console.log(answer);
