4500aeeb5ccd569d2143fed12254103f20d3b80e
Marni Hager <mlh6118@gmail.com>
2021-11-18 22:08:42 -0800

    Adding jack-in-the-box game.

--- js/jackInTheBox.js
+++ js/jackInTheBox.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// Global variables.
+let starImage1 = document.getElementById('star1');
+let starImage2 = document.getElementById('star2');
+let starImage3 = document.getElementById('star3');
+let jackBoxOpenClose = document.getElementById('jack-in-the-box');
+
 // Get the Jack in the Box modal
 let modal = document.getElementById('jackModal');
 
@@ -26,19 +32,53 @@ window.onclick = function(event) {
   }
 };
 
+// Constructor for Stars.
+function Star (dom) {
 
+  // Sets up the DOM for adding to the page.
+  this.dom = dom;
 
+  this.renderStars = function() {
+    dom.src = 'imgs/smallStar.png';
+    dom.classList.add('small');
+  };
 
+  this.renderStars();
 
+  // Change star image when clicked.
+  dom.onclick = function(event) {
+    if (dom.className == 'modal-content small') {
+      dom.src = 'imgs/mediumStar.png';
+      dom.classList.remove('small');
+      dom.classList.add('medium');
+    }
+    else if (dom.className == 'modal-content medium') {
+      dom.src = 'imgs/largeStar.png';
+      dom.classList.remove('medium');
+      dom.classList.add('large');
+    }
+    else if (dom.className == 'modal-content large') {
+      dom.src = 'imgs/smallStar.png';
+      dom.classList.remove('large');
+      dom.classList.add('small');
+    }
+    starSizeCheck();
+  };
+}
 
+// Create new Star objects.
+let star1 = new Star(starImage1);
+let star2 = new Star(starImage2);
+let star3 = new Star(starImage3);
 
+// console.log(star1.dom.className);
 
-
-
-// Create array of the three different size stars.
-
-
-// Populate the modal with three stars of the same size.  Each star position is clickable and changes the size of the star.  Must match the front of the Jack in the Box.
-
-
-// When all three stars match, automatically reward the player with an open Jack in the Box and a number in the balloon.
+// Function to check the stars.
+function starSizeCheck() {
+  // alert('You have entered starSizeCheck');
+  if (star1.dom.className === 'modal-content small' && star2.dom.className === 'modal-content large' && star3.dom.className === 'modal-content medium') {
+    alert('You are correct');
+    // Change the jack in the box image.
+    jackBoxOpenClose.src = "imgs/openJack.png";
+  }
+}
